2c5178fe32c6a0c217b908f60e3df30182bdc332
aleoterob <aleoterob@gmail.com>
2026-01-03 20:12:01 -0300

    feat: add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import userRoutes from './modules/user/user.routes';
 import authRoutes from './modules/auth/auth.routes';
 import { errorMiddleware } from './middleware/error.middleware';
 
 const app = express();
 
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? process.env.FRONTEND_URL
+      : 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/user', userRoutes);
